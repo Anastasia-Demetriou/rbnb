a29b32562c5ca05364c4102f758df4098557b004
Anastasia Demetriou <anastasiademetriou.ad@gmail.com>
2018-11-13 14:44:38 +0000

    home page banner and cards added

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users do
-    resources :events
-  end
+  resources :users
+  resources :events
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
